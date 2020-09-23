bf843564019b5be7ff865c8a91c724ff75d6ca3b
Nikita Karamov <nick@karamoff.dev>
2020-09-23 18:21:33 +0200

    Add post text

--- src/script/index.js
+++ src/script/index.js
@@ -3,14 +3,15 @@ if (remembered != null) {
     document.getElementById('instance').value = remembered;
 }
 
-var parsedQuery = {};
-
 var paramPairs = window.location.search.substr(1).split('&');
 var paramPairsLength = paramPairs.length;
 
 for (var i = 0; i < paramPairsLength; i++) {
     var paramPair = paramPairs[i].split('=');
-    parsedQuery[paramPair[0]] = paramPair[1];
+    if (paramPair[0] === 'text') {
+        document.getElementById('text').value = decodeURIComponent(paramPair[1]);
+        break;
+    }
 }
 delete i
 delete paramPair
@@ -19,6 +20,7 @@ document
   .getElementById('form')
   .addEventListener('submit', function (e) {
     e.preventDefault();
+    var text = e.target.elements['text'].value;
     var instance = e.target.elements['instance'].value;
     var remember = e.target.elements['remember'].checked;
 
@@ -34,6 +36,6 @@ document
         window.localStorage.setItem('mastodon_instance', instance);
     }
 
-    var shareUrl = instance + "share?text=" + parsedQuery.text;
+    var shareUrl = instance + "share?text=" + encodeURIComponent(text);
     window.open(shareUrl, '_blank', 'noopener,noreferrer')
 })
