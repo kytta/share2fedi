303b0d1903224887ff7ccd0b764e0dd4f63870f8
Andy Balaam <andybalaam@artificialworlds.net>
2020-09-24 02:08:07 +0100

    Provide a list of Mastodon servers from joinmastodon.org

--- src/script/index.js
+++ src/script/index.js
@@ -8,6 +8,8 @@ function normalizeUrl(url) {
     return url;
 }
 
+const instance = document.getElementById('instance');
+const choose_instance = document.getElementById('choose_instance');
 var prefillInstance = window.localStorage.getItem('mastodon_instance');
 
 var paramPairs = window.location.search.substr(1).split('&');
@@ -24,9 +26,57 @@ for (var i = 0; i < paramPairsLength; i++) {
 delete i
 delete paramPair
 
-if (prefillInstance != null) {
-    document.getElementById('instance').value = normalizeUrl(prefillInstance);
+function add_instance(text, disabled, selected) {
+    const opt = document.createElement('option');
+    opt.innerText = text;
+    if (disabled) {
+        opt.setAttribute('disabled', true);
+    }
+    if (selected) {
+        opt.setAttribute('selected', true);
+    }
+    choose_instance.appendChild(opt);
 }
+
+if (prefillInstance != null) {
+    const url = normalizeUrl(prefillInstance);
+    instance.value = url;
+    add_instance(url.slice(8, url.length - 1), false, true);
+}
+
+choose_instance.addEventListener('focus', function (e) {
+    if (choose_instance.children.length < 3) {
+        if (choose_instance.children.length > 1) {
+            choose_instance.children[1].remove();
+        }
+        add_instance("... loading ...", true);
+        fetch('https://api.joinmastodon.org/servers')
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error('Failure response from joinmastodon.');
+              }
+              return response.json();
+          })
+          .then(servers => {
+              choose_instance.children[1].remove();
+              const domains = servers.map(obj => obj.domain);
+              domains.sort();
+              for (const domain of domains) {
+                  add_instance(domain);
+              }
+          })
+          .catch(error => {
+              choose_instance.children[1].remove();
+              add_instance("--LOADING FAILED--", true);
+              console.error(
+                'Failed to fetch servers list from joinmastodon.', error);
+          });
+    }
+})
+
+choose_instance.addEventListener('change', function (e) {
+    instance.value = `https://${choose_instance.value}`;
+});
 
 document
   .getElementById('form')
@@ -42,4 +92,4 @@ document
 
     var shareUrl = instance + "share?text=" + encodeURIComponent(text);
     window.open(shareUrl, '_blank', 'noopener,noreferrer')
-})
+  })
