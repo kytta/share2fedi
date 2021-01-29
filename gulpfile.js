a22563ce6d808a60ff475969df3d94cb07972805
Nikita Karamov <nick@karamoff.dev>
2021-01-29 22:56:37 +0100

    Add EditorConfig
    Indentation also changed to tabs

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,34 @@ const terser = require('gulp-terser');
 sass.compiler = require('sass');
 
 function html() {
-    return gulp.src('./src/index.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('./dist/'));
+	return gulp.src('./src/index.pug')
+		.pipe(pug())
+		.pipe(gulp.dest('./dist/'));
 }
 
 function css() {
-    return gulp.src('./src/style/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([require('autoprefixer'), require('cssnano')]))
-        .pipe(gulp.dest('./dist/'));
+	return gulp.src('./src/style/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([require('autoprefixer'), require('cssnano')]))
+		.pipe(gulp.dest('./dist/'));
 }
 
 function js() {
-    return gulp.src('./src/script/index.js')
-        .pipe(terser({ ecma: 5 }))
-        .pipe(gulp.dest('./dist/'));
+	return gulp.src('./src/script/index.js')
+		.pipe(terser({ ecma: 5 }))
+		.pipe(gulp.dest('./dist/'));
 }
 
 function static() {
-    return gulp.src('./src/static/**/*')
-        .pipe(gulp.dest('./dist/'));
+	return gulp.src('./src/static/**/*')
+		.pipe(gulp.dest('./dist/'));
 }
 
 exports.default = gulp.parallel(html, css, js, static);
 
 exports.watch = () => {
-    gulp.watch('./src/index.pug', html);
-    gulp.watch('./src/style/*.scss', css);
-    gulp.watch('./src/script/index.js', js);
-    gulp.watch('./src/static/*', static);
+	gulp.watch('./src/index.pug', html);
+	gulp.watch('./src/style/*.scss', css);
+	gulp.watch('./src/script/index.js', js);
+	gulp.watch('./src/static/*', static);
 }
--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,11 @@
 function normalizeUrl(url) {
-    if (url.indexOf("http://") == -1 && url.indexOf("https://") == -1) {
-        url = "https://" + url;
-    }
-    if (url.charAt(url.length - 1) !== '/'){
-        url = url + "/";
-    }
-    return url;
+	if (url.indexOf("http://") == -1 && url.indexOf("https://") == -1) {
+		url = "https://" + url;
+	}
+	if (url.charAt(url.length - 1) !== '/'){
+		url = url + "/";
+	}
+	return url;
 }
 
 const instance = document.getElementById('instance');
@@ -16,68 +16,68 @@ var prefillInstance = window.localStorage.getItem('mastodon_instance');
 var paramPairs = window.location.search.substr(1).split('&');
 var paramPairsLength = paramPairs.length;
 for (var i = 0; i < paramPairsLength; i++) {
-    var paramPair = paramPairs[i].split('=');
-    if (paramPair[0] === 'text') {
-        document.getElementById('text').value = decodeURIComponent(paramPair[1]);
-    } else if (paramPair[0] === 'instance') {
-        prefillInstance = decodeURIComponent(paramPair[1]);
-    }
+	var paramPair = paramPairs[i].split('=');
+	if (paramPair[0] === 'text') {
+		document.getElementById('text').value = decodeURIComponent(paramPair[1]);
+	} else if (paramPair[0] === 'instance') {
+		prefillInstance = decodeURIComponent(paramPair[1]);
+	}
 }
 delete i
 delete paramPair
 
 function instances_loading_error() {
-    console.error('Failed to fetch servers list from joinmastodon.');
+	console.error('Failed to fetch servers list from joinmastodon.');
 }
 
 function instances_loaded() {
-    if (this.status !== 200) {
-        instances_loading_error();
-        return;
-    }
+	if (this.status !== 200) {
+		instances_loading_error();
+		return;
+	}
 
-    const servers = JSON.parse(this.responseText);
+	const servers = JSON.parse(this.responseText);
 
-    const chosen_instance = instance.value;
-    const domains = servers.map(obj => obj.domain);
-    if (chosen_instance && domains.indexOf(chosen_instance) === -1) {
-        domains.push(chosen_instance);
-    }
-    domains.sort();
+	const chosen_instance = instance.value;
+	const domains = servers.map(obj => obj.domain);
+	if (chosen_instance && domains.indexOf(chosen_instance) === -1) {
+		domains.push(chosen_instance);
+	}
+	domains.sort();
 
-    for (const domain of domains) {
-        const opt = document.createElement('option');
-        opt.value = normalizeUrl(domain);
-        instances_list.appendChild(opt);
-    }
+	for (const domain of domains) {
+		const opt = document.createElement('option');
+		opt.value = normalizeUrl(domain);
+		instances_list.appendChild(opt);
+	}
 }
 
 if (prefillInstance != null) {
-    instance.value = normalizeUrl(prefillInstance);
+	instance.value = normalizeUrl(prefillInstance);
 }
 
 instance.addEventListener('focus', function (e) {
-    if (instances_list.children.length === 0) {
-        const req = new XMLHttpRequest();
-        req.addEventListener('load', instances_loaded);
-        req.addEventListener('error', instances_loading_error);
-        req.open('GET', 'https://api.joinmastodon.org/servers');
-        req.send();
-    }
+	if (instances_list.children.length === 0) {
+		const req = new XMLHttpRequest();
+		req.addEventListener('load', instances_loaded);
+		req.addEventListener('error', instances_loading_error);
+		req.open('GET', 'https://api.joinmastodon.org/servers');
+		req.send();
+	}
 })
 
 document
   .getElementById('form')
   .addEventListener('submit', function (e) {
-    e.preventDefault();
-    var text = e.target.elements['text'].value;
-    var instance = normalizeUrl(e.target.elements['instance'].value);
-    var remember = e.target.elements['remember'].checked;
+		e.preventDefault();
+		var text = e.target.elements['text'].value;
+		var instance = normalizeUrl(e.target.elements['instance'].value);
+		var remember = e.target.elements['remember'].checked;
 
-    if (remember) {
-        window.localStorage.setItem('mastodon_instance', instance);
-    }
+		if (remember) {
+			window.localStorage.setItem('mastodon_instance', instance);
+		}
 
-    var shareUrl = instance + "share?text=" + encodeURIComponent(text);
-    window.location.href = shareUrl;
+		var shareUrl = instance + "share?text=" + encodeURIComponent(text);
+		window.location.href = shareUrl;
   })
