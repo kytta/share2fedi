67808803a403fe4ae4e3c4d3d3a448b24991378f
Nikita Karamov <nick@karamoff.dev>
2021-08-14 19:12:42 +0200

    Use Prettier to format files

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,41 @@
-const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
-const terser = require('gulp-terser');
-
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const terser = require("gulp-terser");
 
 function html() {
-	return gulp.src('./src/index.html')
-		.pipe(gulp.dest('./dist/'));
+	return gulp.src("./src/index.html").pipe(gulp.dest("./dist/"));
 }
 
 function css() {
-	return gulp.src('./src/scss/*.scss')
+	return gulp
+		.src("./src/scss/*.scss")
 		.pipe(sourcemaps.init())
-		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(sass.sync().on("error", sass.logError))
 		.pipe(postcss([require("autoprefixer"), require("postcss-csso")]))
-    .pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./dist/'));
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("./dist/"));
 }
 
 function js() {
-	return gulp.src('./src/main.js')
+	return gulp
+		.src("./src/main.js")
 		.pipe(sourcemaps.init())
 		.pipe(terser({ ecma: 5 }))
-    .pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./dist/'));
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("./dist/"));
 }
 
 function static() {
-	return gulp.src('./src/static/**/*')
-		.pipe(gulp.dest('./dist/'));
+	return gulp.src("./src/static/**/*").pipe(gulp.dest("./dist/"));
 }
 
 exports.default = gulp.parallel(html, css, js, static);
 
 exports.watch = () => {
-	gulp.watch('./src/index.pug', html);
-	gulp.watch('./src/style/*.scss', css);
-	gulp.watch('./src/script/index.js', js);
-	gulp.watch('./src/static/*', static);
-}
+	gulp.watch("./src/index.pug", html);
+	gulp.watch("./src/style/*.scss", css);
+	gulp.watch("./src/script/index.js", js);
+	gulp.watch("./src/static/*", static);
+};
--- src/main.js
+++ src/main.js
@@ -20,32 +20,30 @@ function normalizeUrl(url) {
 	if (url.indexOf("http://") == -1 && url.indexOf("https://") == -1) {
 		url = "https://" + url;
 	}
-	if (url.charAt(url.length - 1) !== '/'){
+	if (url.charAt(url.length - 1) !== "/") {
 		url = url + "/";
 	}
 	return url;
 }
 
-const instance = document.getElementById('instance');
-const instances_list = document.getElementById('instances_list');
+const instance = document.getElementById("instance");
+const instances_list = document.getElementById("instances_list");
 
-var prefillInstance = window.localStorage.getItem('mastodon_instance');
+var prefillInstance = window.localStorage.getItem("mastodon_instance");
 
-var paramPairs = window.location.search.substr(1).split('&');
+var paramPairs = window.location.search.substr(1).split("&");
 var paramPairsLength = paramPairs.length;
 for (var i = 0; i < paramPairsLength; i++) {
-	var paramPair = paramPairs[i].split('=');
-	if (paramPair[0] === 'text') {
-		document.getElementById('text').value = decodeURIComponent(paramPair[1]);
-	} else if (paramPair[0] === 'instance') {
+	var paramPair = paramPairs[i].split("=");
+	if (paramPair[0] === "text") {
+		document.getElementById("text").value = decodeURIComponent(paramPair[1]);
+	} else if (paramPair[0] === "instance") {
 		prefillInstance = decodeURIComponent(paramPair[1]);
 	}
 }
-delete i
-delete paramPair
 
 function instances_loading_error() {
-	console.error('Failed to fetch servers list from joinmastodon.');
+	console.error("Failed to fetch servers list from joinmastodon.");
 }
 
 function instances_loaded() {
@@ -57,14 +55,14 @@ function instances_loaded() {
 	const servers = JSON.parse(this.responseText);
 
 	const chosen_instance = instance.value;
-	const domains = servers.map(obj => obj.domain);
+	const domains = servers.map((obj) => obj.domain);
 	if (chosen_instance && domains.indexOf(chosen_instance) === -1) {
 		domains.push(chosen_instance);
 	}
 	domains.sort();
 
 	for (const domain of domains) {
-		const opt = document.createElement('option');
+		const opt = document.createElement("option");
 		opt.value = normalizeUrl(domain);
 		instances_list.appendChild(opt);
 	}
@@ -74,28 +72,26 @@ if (prefillInstance != null) {
 	instance.value = normalizeUrl(prefillInstance);
 }
 
-instance.addEventListener('focus', function (e) {
+instance.addEventListener("focus", function (e) {
 	if (instances_list.children.length === 0) {
 		const req = new XMLHttpRequest();
-		req.addEventListener('load', instances_loaded);
-		req.addEventListener('error', instances_loading_error);
-		req.open('GET', 'https://api.joinmastodon.org/servers');
+		req.addEventListener("load", instances_loaded);
+		req.addEventListener("error", instances_loading_error);
+		req.open("GET", "https://api.joinmastodon.org/servers");
 		req.send();
 	}
-})
+});
 
-document
-  .getElementById('form')
-  .addEventListener('submit', function (e) {
-		e.preventDefault();
-		var text = e.target.elements['text'].value;
-		var instance = normalizeUrl(e.target.elements['instance'].value);
-		var remember = e.target.elements['remember'].checked;
+document.getElementById("form").addEventListener("submit", function (e) {
+	e.preventDefault();
+	var text = e.target.elements["text"].value;
+	var instance = normalizeUrl(e.target.elements["instance"].value);
+	var remember = e.target.elements["remember"].checked;
 
-		if (remember) {
-			window.localStorage.setItem('mastodon_instance', instance);
-		}
+	if (remember) {
+		window.localStorage.setItem("mastodon_instance", instance);
+	}
 
-		var shareUrl = instance + "share?text=" + encodeURIComponent(text);
-		window.location.href = shareUrl;
-  })
+	var shareUrl = instance + "share?text=" + encodeURIComponent(text);
+	window.location.href = shareUrl;
+});
