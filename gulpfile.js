0a546bdf5046761eda7aea6ece300951ee4b354b
Nikita Karamov <me@kytta.dev>
2023-03-01 12:12:17 +0100

    Set up linters

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
-const { join, resolve } = require("path");
+const path = require("path");
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const terser = require("gulp-terser");
 
-const SOURCE_DIR = resolve(__dirname, "src");
-const OUTPUT_DIR = resolve(__dirname, "public");
+const SOURCE_DIR = path.resolve(__dirname, "src");
+const OUTPUT_DIR = path.resolve(__dirname, "public");
 
 function html() {
-	return gulp.src(join(SOURCE_DIR, "index.html")).pipe(gulp.dest(OUTPUT_DIR));
+	return gulp
+		.src(path.join(SOURCE_DIR, "index.html"))
+		.pipe(gulp.dest(OUTPUT_DIR));
 }
 
 function css() {
 	return gulp
-		.src(join(SOURCE_DIR, "scss", "*.scss"))
+		.src(path.join(SOURCE_DIR, "scss", "*.scss"))
 		.pipe(sourcemaps.init())
 		.pipe(sass.sync().on("error", sass.logError))
 		.pipe(postcss([require("autoprefixer"), require("postcss-csso")]))
@@ -24,24 +26,24 @@ function css() {
 
 function js() {
 	return gulp
-		.src([join(SOURCE_DIR, "main.js"), join(SOURCE_DIR, "count.js")])
+		.src([path.join(SOURCE_DIR, "main.js"), path.join(SOURCE_DIR, "count.js")])
 		.pipe(sourcemaps.init())
 		.pipe(terser({ ecma: 5 }))
 		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest(OUTPUT_DIR));
 }
 
-function static() {
+function staticFiles() {
 	return gulp
-		.src(join(SOURCE_DIR, "static", "**", "*"))
+		.src(path.join(SOURCE_DIR, "static", "**", "*"))
 		.pipe(gulp.dest(OUTPUT_DIR));
 }
 
-exports.default = gulp.parallel(html, css, js, static);
+exports.default = gulp.parallel(html, css, js, staticFiles);
 
 exports.watch = () => {
-	gulp.watch(join(SOURCE_DIR, "index.html"), html);
-	gulp.watch(join(SOURCE_DIR, "scss", "*.scss"), css);
-	gulp.watch(join(SOURCE_DIR, "*.js"), js);
-	gulp.watch(join(SOURCE_DIR, "static", "**", "*"), static);
+	gulp.watch(path.join(SOURCE_DIR, "index.html"), html);
+	gulp.watch(path.join(SOURCE_DIR, "scss", "*.scss"), css);
+	gulp.watch(path.join(SOURCE_DIR, "*.js"), js);
+	gulp.watch(path.join(SOURCE_DIR, "static", "**", "*"), staticFiles);
 };
