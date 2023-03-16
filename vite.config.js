9eb9be57673e7c218d9011363f55bde5f228fb6f
Nikita Karamov <me@kytta.dev>
2023-03-16 13:00:18 +0100

    Fix Vite config
    Node plugin would also work on build which messes up the `root` config
    entry. This should fix the deploy not working.

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
 import legacy from "@vitejs/plugin-legacy";
 import { VitePluginNode } from "vite-plugin-node";
 
+const vitePluginNode = VitePluginNode({
+	// Workaround from: https://github.com/axe-me/vite-plugin-node/issues/47
+	adapter({ app, req, res, next }) {
+		if (req.url.startsWith("/api/")) {
+			app(req, res);
+		} else {
+			next();
+		}
+	},
+	appPath: "./api/share.js",
+});
+vitePluginNode[0].apply = "serve";
+
 export default {
 	build: {
 		minify: "terser",
 		terserOptions: { ecma: 5 },
 		sourcemap: "true",
 	},
-	plugins: [
-		legacy(),
-		VitePluginNode({
-			// Workaround from: https://github.com/axe-me/vite-plugin-node/issues/47
-			adapter({ app, req, res, next }) {
-				if (req.url.startsWith("/api/")) {
-					app(req, res);
-				} else {
-					next();
-				}
-			},
-			appPath: "./api/share.js",
-		}),
-	],
+	plugins: [legacy(), ...vitePluginNode],
 };
