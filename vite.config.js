7961201f953d659a7be5fa00bb842173f7bace4b
Nikita Karamov <me@kytta.dev>
2023-03-16 12:18:17 +0100

    Add vite-plugin-node to be able to run API locally

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import legacy from "@vitejs/plugin-legacy";
+import { VitePluginNode } from "vite-plugin-node";
 
 export default {
 	build: {
@@ -6,5 +7,18 @@ export default {
 		terserOptions: { ecma: 5 },
 		sourcemap: "true",
 	},
-	plugins: [legacy()],
+	plugins: [
+		legacy(),
+		VitePluginNode({
+			// Workaround from: https://github.com/axe-me/vite-plugin-node/issues/47
+			adapter({ app, req, res, next }) {
+				if (req.url.startsWith("/api/")) {
+					app(req, res);
+				} else {
+					next();
+				}
+			},
+			appPath: "./api/share.js",
+		}),
+	],
 };
