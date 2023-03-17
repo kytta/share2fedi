c52761a4e8161bedeb48157480e9d90628076133
Nikita Karamov <me@kytta.dev>
2023-03-17 23:52:42 +0100

    Re-implement saving instances
    Also implements saving of multiple instances, which
    closes https://github.com/kytta/share2fedi/issues/29

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,19 @@
+/**
+ * Adds missing "https://" and ending slash to the URL
+ *
+ * @param url URL to normalize
+ * @return normalized URL
+ */
+export const normalizeURL = (url: string): string => {
+	if (!(url.startsWith("https://") || url.startsWith("http://"))) {
+		url = "https://" + url;
+	}
+	if (!url.endsWith("/")) {
+		url += "/";
+	}
+	return url;
+};
+
 export const extractHost = (url: string): string => {
 	if (!(url.startsWith("https://") || url.startsWith("http://"))) {
 		url = "https://" + url;
