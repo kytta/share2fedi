99f154bb63cd8f61b3d4cece3d1f0fbed4d439b5
Nikita Karamov <me@kytta.dev>
2023-03-18 03:03:55 +0100

    Return empty list if no instances could be fetched

--- src/pages/api/instances.ts
+++ src/pages/api/instances.ts
@@ -6,16 +6,25 @@
 import { APIRoute } from "astro";
 
 export const get: APIRoute = async () => {
-	const response = await fetch("https://api.joinmastodon.org/servers");
-	const instances = await response.json();
+	try {
+		const response = await fetch("https://api.joinmastodon.org/servers");
+		const instances = await response.json();
 
-	return new Response(
-		JSON.stringify(instances.map((instance) => instance.domain)),
-		{
+		return new Response(
+			JSON.stringify(instances.map((instance) => instance.domain)),
+			{
+				headers: {
+					"Cache-Control": "s-maxage=86400, max-age=86400, public",
+					"Content-Type": "application/json",
+				},
+			},
+		);
+	} catch (error) {
+		console.error("Could not fetch instances:", error);
+		return new Response(JSON.stringify([]), {
 			headers: {
-				"Cache-Control": "s-maxage=86400, max-age=86400, public",
 				"Content-Type": "application/json",
 			},
-		},
-	);
+		});
+	}
 };
