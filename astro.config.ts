3d3df82491e4d934d50dbd0782e027b45caa1aff
Nikita Karamov <me@kytta.dev>
2023-03-18 03:12:42 +0100

    Try to make Cloudflare deploy its config and not Netlify

--- astro.config.ts
+++ astro.config.ts
@@ -6,15 +6,15 @@ import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
 
 let astroAdapter;
-if (process.env.VERCEL) {
+if (process.env.CF_PAGES) {
+	console.debug("Using Cloudflare adapter");
+	astroAdapter = cloudflare();
+} else if (process.env.VERCEL) {
 	console.debug("Using Vercel adapter");
 	astroAdapter = vercel();
 } else if (process.env.NETLIFY) {
 	console.debug("Using Netlify adapter");
 	astroAdapter = netlify();
-} else if (process.env.CF_PAGES) {
-	console.debug("Using Cloudflare adapter");
-	astroAdapter = cloudflare();
 } else {
 	console.debug("Using Node.js adapter");
 	astroAdapter = node({
