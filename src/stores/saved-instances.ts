870406209e4ac99f24b6a255e608854467b5fe4f
Nikita Karamov <me@kytta.dev>
2023-08-26 15:56:50 +0200

    Rewrite nanostore for saved instances

--- src/stores/saved-instances.ts
+++ src/stores/saved-instances.ts
@@ -1,33 +1,40 @@
 import { persistentAtom } from "@nanostores/persistent";
 import { getUrlDomain } from "@scripts/util";
+import { action, onMount } from "nanostores";
 
-const LOCAL_STORAGE_KEY = "recentInstances";
-const RECENT_INSTANCES_SIZE = 5;
+const OLD_LOCAL_STORAGE_KEY = "recentInstances";
+const LOCAL_STORAGE_KEY = "savedInstances";
+const CAPACITY = 5;
 
-export const savedInstances = persistentAtom<Set<string>>(
+export const $savedInstances = persistentAtom<Set<string>>(
 	LOCAL_STORAGE_KEY,
 	new Set(),
 	{
 		encode: (set) => JSON.stringify([...set]),
-		decode: (value) => {
-			return new Set(
-				// XXX: The conversion to a domain need to be done to support legacy
-				//  users, who may have full URLs in their Storage
-				JSON.parse(value).map((instanceUrl: string) =>
-					getUrlDomain(instanceUrl),
-				),
-			);
-		},
+		decode: (value) => new Set(JSON.parse(value)),
 	},
 );
 
-export const saveInstance = (instance: string) => {
-	savedInstances.set(
+onMount($savedInstances, () => {
+	// XXX: The conversion to a domain need to be done to support legacy
+	//  users, who may have full URLs in their Storage
+	const oldItem = localStorage.getItem(OLD_LOCAL_STORAGE_KEY);
+	if (!oldItem) {
+		return;
+	}
+
+	$savedInstances.set(
 		new Set(
-			[getUrlDomain(instance), ...savedInstances.get()].slice(
-				0,
-				RECENT_INSTANCES_SIZE,
+			JSON.parse(oldItem).map((instanceUrl: string) =>
+				getUrlDomain(instanceUrl),
 			),
 		),
 	);
-};
+	localStorage.removeItem(OLD_LOCAL_STORAGE_KEY);
+});
+
+export const save = action($savedInstances, "save", (store, instance) => {
+	store.set(
+		new Set([getUrlDomain(instance), ...store.get()].slice(0, CAPACITY)),
+	);
+});
