dbc53bd9a3a7bcc0363d6cb7e64bffd3df56f2c7
Nikita Karamov <me@kytta.dev>
2026-03-22 16:14:56 +0100

    Upgrade config

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,21 @@
 //
 // SPDX-FileCopyrightText: © 2023 Nikita Karamov <me@kytta.dev>
 // SPDX-License-Identifier: CC0-1.0
-import globals from "globals";
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
 import astro from "eslint-plugin-astro";
 import unicorn from "eslint-plugin-unicorn";
-import prettier from "eslint-config-prettier";
+import prettier from "eslint-config-prettier/flat";
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
-export default [
+export default defineConfig(
 	{
 		ignores: [".astro", "dist/", "script/"],
 	},
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-			},
-		},
-	},
 	js.configs.recommended,
-	...ts.configs.recommended,
-	unicorn.configs["flat/recommended"],
+	ts.configs.recommended,
+	unicorn.configs.recommended,
 	...astro.configs["jsx-a11y-strict"],
 	prettier,
-];
+);
